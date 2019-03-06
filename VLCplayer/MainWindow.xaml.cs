37b7454b459ee4f78295738bdfc2ad17b2ae16eb
Jan Převrátil <honzaprevratil@seznam.cz>
2019-03-06 09:30:26 +0100

    pokus o binding :(

--- VLCplayer/MainWindow.xaml.cs
+++ VLCplayer/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Path = System.IO.Path;
 using Vlc.DotNet;
+using System.Diagnostics;
 
 namespace VLCplayer
 {
@@ -23,20 +24,39 @@ namespace VLCplayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        VideoVM VideoVM { get; set; } = new VideoVM();
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = VideoVM;
 
             var CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
             var vlcLibDirectory = new DirectoryInfo(Path.Combine(CurrentDirectory, "libvlc", IntPtr.Size == 4 ? "win-x86" : "win-x64"));
 
             MyControl.MediaPlayer.VlcLibDirectory = vlcLibDirectory;
             MyControl.MediaPlayer.EndInit();
-            MyControl.MediaPlayer.Play(new Uri(@"D:\source\repos\VLCplayer\videos\big_buck_bunny_480p_h264.mov"));
+
+            MyControl.MediaPlayer.TimeChanged += MediaPlayer_TimeChanged;
+            MyControl.MediaPlayer.LengthChanged += MediaPlayer_LengthChanged;
+
+            PlayVideo(new Uri(@"D:\source\repos\VLCplayer\videos\big_buck_bunny_480p_h264.mov"));
         }
 
-        public void PlayVideo()
+        private void MediaPlayer_LengthChanged(object sender, Vlc.DotNet.Core.VlcMediaPlayerLengthChangedEventArgs e)
         {
+            VideoVM.VideoLength = e.NewLength;
+            Debug.WriteLine(e.NewLength);
+        }
+
+        private void MediaPlayer_TimeChanged(object sender, Vlc.DotNet.Core.VlcMediaPlayerTimeChangedEventArgs e)
+        {
+            VideoVM.VideoTime = e.NewTime;
+        }
+
+        public void PlayVideo(Uri UriOfVideo)
+        {
+            MyControl.MediaPlayer.Play(UriOfVideo);
         }
 
         private void PauseButton_Click(object sender, RoutedEventArgs e)
@@ -51,16 +71,29 @@ namespace VLCplayer
                 MyControl.MediaPlayer.Play();
                 PauseButtonImage.Source = new BitmapImage(new Uri("pack://application:,,,/VLCplayer;component/pictures/pause.png"));
             }
+
+            Debug.WriteLine(TimeSlider.Value);
+        }
+
+        private void ForwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyControl.MediaPlayer.Time += 5000;
+        }
+
+        private void BackwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyControl.MediaPlayer.Time -= 5000;
         }
 
         private void MenuItem_Open(object sender, RoutedEventArgs e)
         {
             // Create OpenFileDialog 
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-
-            // Set filter for file extension and default file extension 
-            dlg.DefaultExt = ".png";
-            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                // Set filter for file extension and default file extension 
+                DefaultExt = ".mov",
+                Filter = "MOV Files (*.mov)|*.mov|MP4 Files (*.mp4)|*.mp4"
+            };
 
             // Display OpenFileDialog by calling ShowDialog method 
             bool? result = dlg.ShowDialog();
@@ -68,7 +101,8 @@ namespace VLCplayer
             // Get the selected file name and display in a TextBox 
             if (result == true)
             {
-
+                Console.WriteLine(dlg.FileName);
+                PlayVideo(new Uri(dlg.FileName));
             }
         }
     }
---
+++ VLCplayer/VideoVM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VLCplayer
+{
+    class VideoVM : INotifyPropertyChanged
+    {
+        /* Video Length = Slider Maximum */
+        private float videoLength;
+        public float VideoLength
+        {
+            get { return videoLength; }
+            set
+            {
+                videoLength = value;
+                OnPropertyChanged("VideoLength");
+            }
+        }
+
+        /* Video current Time = Slider Value */
+        private long videoTime;
+        public long VideoTime
+        {
+            get { return videoTime; }
+            set
+            {
+                videoTime = value;
+                OnPropertyChanged("VideoTime");
+            }
+        }
+
+        /* EVENT AND FUNCTION FOR EVENT */
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
